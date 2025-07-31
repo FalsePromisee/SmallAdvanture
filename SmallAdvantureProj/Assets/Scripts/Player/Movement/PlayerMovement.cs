3009853c3d915a0a261667d60778b37e75948b23
Павел Галковский <pasha.galkovsky22@gmail.com>
2025-07-31 19:04:03 +0300

    Jump
    Trying to make a workable jump

--- SmallAdvantureProj/Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ SmallAdvantureProj/Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -1,10 +1,13 @@
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEngine.UI.Image;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed = 10.0f;
+    [SerializeField]private float _jumpForce = 5.0f;
+
     [SerializeField] private float _damping = 0.1f;
 
     [SerializeField] private InputActionReference _moveInput;
@@ -15,6 +18,9 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 _moveDirection;
     private float _currentVelocity;
 
+    private float _sphereRadius = 0.1f; 
+
+
 
     private void Start()
     {
@@ -24,6 +30,7 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         PlayerInputReading();
+        Debug.DrawLine(this.transform.position, Vector3.down * 0.15f);
     }
 
 
@@ -35,10 +42,9 @@ public class PlayerMovement : MonoBehaviour
     private void PlayerInputReading()
     {
         _moveDirection = _moveInput.action.ReadValue<Vector2>();
-        if (_jumpInput.action.triggered)
+        if (_jumpInput.action.triggered && IsGrounded())
         {
-            // Handle jump logic here
-            Debug.Log("Jump triggered");
+            _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
         }
     }
     private void AddMovement()
@@ -47,6 +53,17 @@ public class PlayerMovement : MonoBehaviour
         _rigidbody.linearVelocity = new Vector3(_currentVelocity, 0.0f);
     }
 
+    private bool IsGrounded()
+    {
+        Ray ray = new Ray(this.transform.position, Vector3.down * 0.1f);
+        RaycastHit hitInfo;
+        if (Physics.SphereCast(ray, _sphereRadius, out hitInfo) && hitInfo.collider.CompareTag("Ground") )
+        {
+            return true;
+        }
+        return false;
+
+    }
 
 
 }
