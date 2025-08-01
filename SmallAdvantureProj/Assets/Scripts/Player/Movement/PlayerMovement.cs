13f619547322b0367624dac9f83a1ea4aa952854
Павел Галковский <pasha.galkovsky22@gmail.com>
2025-08-01 20:15:23 +0300

    Trying to fix jump and fall

--- SmallAdvantureProj/Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ SmallAdvantureProj/Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -1,63 +1,67 @@
-using Unity.VisualScripting;
+
 using UnityEngine;
 using UnityEngine.InputSystem;
-using static UnityEngine.UI.Image;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed = 10.0f;
-    [SerializeField]private float _jumpForce = 5.0f;
-
-    [SerializeField] private float _damping = 0.1f;
-
-    [SerializeField] private InputActionReference _moveInput;
-    [SerializeField] private InputActionReference _jumpInput;
+    [SerializeField] private float _jumpForce = 5.0f;
+    [SerializeField] private float _dampingSpeed = 0.1f;
 
     private Rigidbody _rigidbody;
+
+    
+    private InputAction _moveInput;
+    private Inputs _inputsAction;
+
 
     private Vector2 _moveDirection;
     private float _currentVelocity;
 
-    private float _sphereRadius = 0.1f; 
+    private float _raycastDistance = 1.35f; 
+    [SerializeField] private float _extraFallGravity = 2.0f; 
 
 
 
-    private void Start()
+    private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _inputsAction = new Inputs();
     }
 
     private void Update()
     {
         PlayerInputReading();
-        Debug.DrawLine(this.transform.position, Vector3.down * 0.15f);
     }
 
 
     private void FixedUpdate()
     {
         AddMovement();
+
+
+        if (_rigidbody.linearVelocity.y < 0f)
+        {
+            _rigidbody.linearVelocity += Vector3.down * _extraFallGravity * Time.fixedDeltaTime;            
+        }
+        
     }
 
-    private void PlayerInputReading()
+    private void OnEnable()
     {
-        _moveDirection = _moveInput.action.ReadValue<Vector2>();
-        if (_jumpInput.action.triggered && IsGrounded())
-        {
-            _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
-        }
+        _moveInput = _inputsAction.WalkControl.Move;        
+        _inputsAction.WalkControl.Enable();
     }
-    private void AddMovement()
+
+    private void OnDisable()
     {
-        _currentVelocity = Mathf.Lerp(_currentVelocity, _moveDirection.x * _moveSpeed * Time.fixedDeltaTime, Time.fixedDeltaTime * _damping);
-        _rigidbody.linearVelocity = new Vector3(_currentVelocity, 0.0f);
+        _inputsAction.WalkControl.Disable();
     }
 
     private bool IsGrounded()
     {
-        Ray ray = new Ray(this.transform.position, Vector3.down * 0.1f);
-        RaycastHit hitInfo;
-        if (Physics.SphereCast(ray, _sphereRadius, out hitInfo) && hitInfo.collider.CompareTag("Ground") )
+        Ray ray = new Ray(this.transform.position + Vector3.up * 0.25f, Vector3.down);        
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, _raycastDistance))
         {
             return true;
         }
@@ -65,5 +69,20 @@ public class PlayerMovement : MonoBehaviour
 
     }
 
+
+    private void PlayerInputReading()
+    {
+        _moveDirection = _inputsAction.WalkControl.Move.ReadValue<Vector2>();
+        if (_inputsAction.WalkControl.Jump.triggered && IsGrounded())
+        {
+            _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void AddMovement()
+    {
+        _currentVelocity = Mathf.Lerp(_currentVelocity, _moveDirection.x * _moveSpeed * Time.fixedDeltaTime, Time.fixedDeltaTime * _dampingSpeed);
+        _rigidbody.linearVelocity = new Vector3(_currentVelocity, 0.0f);
+    }
 
 }
